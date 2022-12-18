ff675d36ce70bce1cba2381f5e1861b7d2c44915
Andrey Borha <diajelovic@gmail.com>
2022-12-18 12:59:17 +0400

    front-fixes

--- apps/front/src/App.tsx
+++ apps/front/src/App.tsx
@@ -1,28 +1,24 @@
-import React, {useEffect} from 'react';
-import {Router, BrowserRouter, Routes, Route, Link} from "react-router-dom"
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import MainPage from './routes/MainPage/MainPage';
+import MainPage from "./routes/MainPage/MainPage";
 
 const App = () => {
-  useEffect(() => {
-    // fetch("http://localhost:3030").then(res => res.text()).then(res => console.log(res)) // Todo: replace to axios
-  }, [])
   return (
     <div className="container app_container">
       <div className="app_wrapper">
-          <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/contacts" element={<h1 className="header-font" style={{color: "#19282F", fontSize: "5vmin", position: "absolute", top: "50%", transform: "translateY: -50%"}}>Page in progress</h1>} />
-                <Route path="/menu" element={<h1 className="header-font" style={{color: "#19282F", fontSize: "5vmin", position: "absolute", top: "50%", transform: "translateY: -50%"}}>Page in progress</h1>} />
-                <Route path="/signin" element={<h1 className="header-font" style={{color: "#19282F", fontSize: "5vmin", position: "absolute", top: "50%", transform: "translateY: -50%"}}>Page in progress</h1>} />
-            </Routes>
-          </BrowserRouter>
-        {/* <MainPage></MainPage> */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            {/*<Route path="/contacts" element={<h1 className="header-font" style={{color: "#19282F", fontSize: "5vmin", position: "absolute", top: "50%", transform: "translateY: -50%"}}>Page in progress</h1>} />*/}
+            {/*<Route path="/menu" element={<h1 className="header-font" style={{color: "#19282F", fontSize: "5vmin", position: "absolute", top: "50%", transform: "translateY: -50%"}}>Page in progress</h1>} />*/}
+            {/*<Route path="/signin" element={<h1 className="header-font" style={{color: "#19282F", fontSize: "5vmin", position: "absolute", top: "50%", transform: "translateY: -50%"}}>Page in progress</h1>} />*/}
+          </Routes>
+        </BrowserRouter>
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- apps/front/src/components/searchBar/SearchBar.tsx
+++ apps/front/src/components/searchBar/SearchBar.tsx
@@ -1,43 +1,100 @@
-import React, {useRef} from 'react'
-import "./SearchBar.css"
-import CalendarIcon from '../../sources/calendar'
-import CalendarRedIcon from '../../sources/calendar_red'
-import CircleRedIcon from '../../sources/circle_red'
-import CircleRedBigIcon from '../../sources/circle_red_big'
-// import {ReactComponent as CalendarIcon} from '../../sources'
+import React from "react";
+import "./SearchBar.css";
+import CalendarIcon from "../../sources/calendar";
+import CalendarRedIcon from "../../sources/calendar_red";
+import CircleRedIcon from "../../sources/circle_red";
+import CircleRedBigIcon from "../../sources/circle_red_big";
+
 type SearchBarPropsType = {
-    onClick: () => {},
-    onSubmit: () => {},
-    onInput: () => {},
-}
-const dateInput = document.querySelector('#date_input')
+  onSubmit: () => {};
+};
+// const dateInput = document.querySelector("#date_input");
+
 const SearchBar = (props: SearchBarPropsType) => {
   function showPopup() {
-    const target = document.querySelector('#date_input') as HTMLInputElement
-    target.showPicker()
+    const target = document.querySelector("#date_input") as HTMLInputElement;
+    target.showPicker();
   }
-  const interviewDateRef = useRef();
-    const handleInterviewDateClick = () => {
-    // interviewDateRef.current.focus();
-  };
+
+  const [from, setFrom] = React.useState("");
+  const [to, setTo] = React.useState("");
+
+  const submit = React.useCallback(
+    (e: React.FormEvent) => {
+      e.preventDefault();
+      console.debug("---from", from);
+      props.onSubmit();
+    },
+    [from]
+  );
+
+  const changeFrom = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFrom(e.currentTarget.value);
+    },
+    []
+  );
+  const changeTo = React.useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setTo(e.currentTarget.value);
+    },
+    []
+  );
+
   return (
-    <form onClick={props.onClick} className='searchbar_form' onSubmit={(e) => {e.preventDefault(); props.onSubmit()}}>
-        <div className='input_section'>
-          <div className='redCircle_wrapper_m'><CircleRedIcon /></div>
-          <div className='redCircle_wrapper_d'><CircleRedBigIcon /></div>
-          <input onInput={props.onInput} placeholder='Откуда' className='searchbar_departure_input' type="text" />
+    <form className="searchbar_form" onSubmit={submit}>
+      <div className="input_section">
+        <div className="redCircle_wrapper_m">
+          <CircleRedIcon />
         </div>
-        <div className='input_section'>
-          <div className='redCircle_wrapper_m'><CircleRedIcon /></div>
-          <div className='redCircle_wrapper_d'><CircleRedBigIcon /></div>
-          <input onInput={props.onInput} placeholder='Куда' className='searchbar_destination_input' type="text" />
+        <div className="redCircle_wrapper_d">
+          <CircleRedBigIcon />
         </div>
-        <input onInput={props.onInput} name='date_input' id="date_input" className='searchbar_date_input' type="date" />
-        <label className='icon_m' htmlFor="date_input"><CalendarRedIcon /></label>
-        <label onClick={() => {showPopup()}} htmlFor="date_input"><CalendarIcon /></label>
-    
-        <button type="submit" className='form_submit_btn text-font'>Найти</button>
+        <input
+          onChange={changeFrom}
+          placeholder="Откуда"
+          className="searchbar_departure_input"
+          value={from}
+          type="text"
+        />
+      </div>
+      <div className="input_section">
+        <div className="redCircle_wrapper_m">
+          <CircleRedIcon />
+        </div>
+        <div className="redCircle_wrapper_d">
+          <CircleRedBigIcon />
+        </div>
+        <input
+          onInput={changeTo}
+          placeholder="Куда"
+          className="searchbar_destination_input"
+          value={to}
+          type="text"
+        />
+      </div>
+      <input
+        name="date_input"
+        id="date_input"
+        className="searchbar_date_input"
+        type="date"
+      />
+      <label className="icon_m" htmlFor="date_input">
+        <CalendarRedIcon />
+      </label>
+      <label
+        onClick={() => {
+          showPopup();
+        }}
+        htmlFor="date_input"
+      >
+        <CalendarIcon />
+      </label>
+
+      <button type="submit" className="form_submit_btn text-font">
+        Найти
+      </button>
     </form>
-  )
-}
-export default SearchBar
+  );
+};
+export default SearchBar;
