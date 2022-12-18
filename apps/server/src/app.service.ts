c826607c8b2d76883bf0034425bee465aee7162d
Andrey Borha <diajelovic@gmail.com>
2022-12-18 11:46:33 +0400

    add git ignore

--- apps/server/src/app.service.ts
+++ apps/server/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable, Inject, CACHE_MANAGER } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import { parse } from 'csv-parse';
 import { join } from 'path';
-import { createReadStream, read } from 'fs';
+import { createReadStream } from 'fs';
 
 const MAX_STATIONS = 10;
 
